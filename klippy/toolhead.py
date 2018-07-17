NordicGuy/klipper

itersolve: Initial support for 4th and 6th order bezier acceleration curves
Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

klippy/toolhead.py
@@ -234,6 +234,9 @@ class ToolHead:
                     'delta': delta.DeltaKinematics}
         self.kin = config.getchoice('kinematics', kintypes)(
             self, printer, config)
+        self.accel_order = config.getchoice(
+            'acceleration_order', { "2": 2, "4": 4, "6": 6 }, "2")
+        self.kin.setup_accel_order(self.accel_order)
         # SET_VELOCITY_LIMIT command
         gcode = printer.lookup_object('gcode')
         gcode.register_command('SET_VELOCITY_LIMIT', self.cmd_SET_VELOCITY_LIMIT,
@@ -370,6 +373,7 @@ class ToolHead:
         self.extruder.set_active(last_move_time, False)
         extrude_pos = extruder.set_active(last_move_time, True)
         self.extruder = extruder
+        self.extruder.setup_accel_order(self.accel_order)
         self.move_queue.set_extruder(extruder)
         self.commanded_pos[3] = extrude_pos
     def get_extruder(self):
@@ -424,16 +428,20 @@ class ToolHead:
             minval=0., maxval=self.config_junction_deviation)
         self.requested_accel_to_decel = gcode.get_float(
             'ACCEL_TO_DECEL', params, self.requested_accel_to_decel, above=0.)
+        accel_order = gcode.get_int(
+            'ACCEL_ORDER', params, self.accel_order, minval=2, maxval=6)
+        if accel_order != self.accel_order:
+            self.accel_order = accel_order
+            self.kin.setup_accel_order(accel_order)
+            self.extruder.setup_accel_order(accel_order)
         self.max_velocity = max_velocity
         self.max_accel = max_accel
         self.max_accel_to_decel = min(self.requested_accel_to_decel, max_accel)
         self.junction_deviation = junction_deviation
-        msg = ("max_velocity: %.6f\n"
-               "max_accel: %.6f\n"
-               "max_accel_to_decel: %.6f\n"
-               "junction_deviation: %.6f"% (
-                   max_velocity, max_accel, self.requested_accel_to_decel,
-                   junction_deviation))
+        msg = ("max_velocity: %.6f max_accel: %.6f accel_order: %d\n"
+               "max_accel_to_decel: %.6f junction_deviation: %.6f"% (
+                   max_velocity, max_accel, accel_order,
+                   self.requested_accel_to_decel, junction_deviation))
         self.printer.set_rollover_info("toolhead", "toolhead: %s" % (msg,))
         gcode.respond_info(msg)
 

yumit412/First

klippy/toolhead.py
@@ -233,9 +233,12 @@ class ToolHead:
         self.move_queue.set_flush_time(self.buffer_time_high)
         self.printer.try_load_module(config, "idle_timeout")
         # Setup iterative solver
-        ffi_main, ffi_lib = chelper.get_ffi()
-        self.cmove = ffi_main.gc(ffi_lib.move_alloc(), ffi_lib.free)
-        self.move_fill = ffi_lib.move_fill
+        ffi_main, self.ffi_lib = chelper.get_ffi()
+        self.cmove = ffi_main.gc(self.ffi_lib.move_alloc(), self.ffi_lib.free)
+        self.move_fill = self.ffi_lib.move_fill
+        self.accel_order = config.getchoice(
+            'acceleration_order', { "2": 2, "4": 4, "6": 6 }, "2")
+        self.ffi_lib.move_set_accel_order(self.cmove, self.accel_order)
         # Create kinematics class
         self.extruder = kinematics.extruder.DummyExtruder()
         self.move_queue.set_extruder(self.extruder)
@@ -370,6 +373,7 @@ class ToolHead:
         self.extruder.set_active(last_move_time, False)
         extrude_pos = extruder.set_active(last_move_time, True)
         self.extruder = extruder
+        self.extruder.setup_accel_order(self.accel_order)
         self.move_queue.set_extruder(extruder)
         self.commanded_pos[3] = extrude_pos
     def get_extruder(self):
@@ -429,17 +433,21 @@ class ToolHead:
             minval=0., maxval=self.config_square_corner_velocity)
         self.requested_accel_to_decel = gcode.get_float(
             'ACCEL_TO_DECEL', params, self.requested_accel_to_decel, above=0.)
+        accel_order = gcode.get_int(
+            'ACCEL_ORDER', params, self.accel_order, minval=2, maxval=6)
+        if accel_order != self.accel_order:
+            self.accel_order = accel_order
+            self.ffi_lib.move_set_accel_order(self.cmove, accel_order)
+            self.extruder.setup_accel_order(accel_order)
         self.max_velocity = max_velocity
         self.max_accel = max_accel
         self.max_accel_to_decel = min(self.requested_accel_to_decel, max_accel)
         self.square_corner_velocity = square_corner_velocity
         self._calc_junction_deviation()
-        msg = ("max_velocity: %.6f\n"
-               "max_accel: %.6f\n"
-               "max_accel_to_decel: %.6f\n"
-               "square_corner_velocity: %.6f"% (
-                   max_velocity, max_accel, self.requested_accel_to_decel,
-                   square_corner_velocity))
+        msg = ("max_velocity: %.6f max_accel: %.6f accel_order: %d\n"
+               "max_accel_to_decel: %.6f square_corner_velocity: %.6f"% (
+                   max_velocity, max_accel, accel_order,
+                   self.requested_accel_to_decel, square_corner_velocity))
         self.printer.set_rollover_info("toolhead", "toolhead: %s" % (msg,))
         gcode.respond_info(msg)
     def cmd_M204(self, params):